484fda63aef5c72d50f679dbb664c73afe1daa40
st0rm348 <brunommsoares@outlook.com>
2024-06-25 15:39:10 +0100

    feat(app): add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import Navbar from './components/navbar/Navbar';
+
 import './App.scss';
 
 const App = () => {
   return (
-    <h1 className='app'>App</h1>
+    <h1 className='app'>
+      <Navbar />
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+    </h1>
   )
 }
 export default App
