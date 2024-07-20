1ffd69bbf87e8cb656e7c0c83d62636313a0943d
st0rm348 <brunommsoares@outlook.com>
2024-07-20 10:21:32 +0100

    refactor: add file URL fetch ability on sanity client

--- frontend/src/lib/client.js
+++ frontend/src/lib/client.js
@@ -1,9 +1,15 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-const client = createClient({
+const projectData = {
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: 'production',
+  baseURL: 'https://cdn.sanity.io/'
+};
+
+const client = createClient({
+  projectId: projectData.projectId,
+  dataset: projectData.dataset,
   apiVersion: '2022-02-01',
   useCdn: true,
   token: import.meta.env.VITE_SANITY_TOKEN
@@ -13,7 +19,16 @@ const builder = imageUrlBuilder(client);
 
 const urlFor = (source) => builder.image(source);
 
+const urlForFile = (file) => {
+  // eslint-disable-next-line no-unused-vars
+  const [_file, id, extension] = file.asset._ref.split('-');
+  const { baseURL, projectId, dataset } = projectData;
+
+  return `${baseURL}files/${projectId}/${dataset}/${id}.${extension}?dl=`;
+};
+
 export {
   client,
-  urlFor
+  urlFor,
+  urlForFile
 };
