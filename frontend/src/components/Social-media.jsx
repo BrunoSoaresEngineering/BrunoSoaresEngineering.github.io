8e5fea0c69e764a48e5d014983249c9ad2823718
st0rm348 <brunommsoares@outlook.com>
2024-07-20 10:54:46 +0100

    feat(app-wrap): add cv download

--- frontend/src/components/Social-media.jsx
+++ frontend/src/components/Social-media.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import { BsGithub } from 'react-icons/bs';
-import { BiLogoLinkedin } from 'react-icons/bi';
+import { BiLogoLinkedin, BiSolidDownload } from 'react-icons/bi';
+
+import { client, urlForFile } from '../lib/client';
 
 const SocialMedia = () => {
+  const [cvFileURL, setCvFileURL] = useState(undefined);
+
+  const fetchCv = async () => {
+    const cvQuery = '*[_type == "cv"][0]';
+
+    const { file } = await client.fetch(cvQuery);
+    setCvFileURL(urlForFile(file));
+  };
+
+  useEffect(() => {
+    fetchCv();
+  }, []);
+
   return (
     <div className="app__social">
+      <a href={cvFileURL}>
+        <div>
+          <BiSolidDownload />
+        </div>
+      </a>
       <a href="https://www.linkedin.com/in/bruno-soares-engineering/" target='_blank'>
         <div>
           <BiLogoLinkedin />
@@ -15,6 +36,7 @@ const SocialMedia = () => {
         </div>
       </a>
     </div>
-  )
-}
+  );
+};
+
 export default SocialMedia;
