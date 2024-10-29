c471a5dccd141c598311170f1142941b547ea4f0
st0rm348 <brunommsoares@outlook.com>
2024-10-29 01:25:56 +0000

    feat(work): add project code links to the modal

--- frontend/src/containers/work/components/Modal.jsx
+++ frontend/src/containers/work/components/Modal.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { PortableText } from '@portabletext/react';
 import { Tooltip } from 'react-tooltip';
+import { motion } from 'framer-motion';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import ModalBase from '../../../components/modal/Modal.jsx';
 import { urlFor } from '../../../lib/client';
 
@@ -17,6 +19,42 @@ function Modal({
         <div className="app__modal">
           <div className="app__modal-img app__flex">
             <img src={urlFor(workItem?.imgUrl)} alt={workItem?.name} />
+            {(workItem?.projectLink || workItem?.codeLink) && (
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                animate={{ opacity: 0 }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__modal-img-hover"
+              >
+                {workItem?.projectLink && (
+                  <a href={workItem?.projectLink} target="_blank" rel="noreferrer">
+
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className='app__flex'
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                )}
+
+                {workItem?.codeLink && (
+                  <a href={workItem?.codeLink} target="_blank" rel="noreferrer">
+
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className='app__flex'
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                )}
+              </motion.div>
+            )}
             <p className="p-text">{workItem?.tags[0]}</p>
           </div>
           <h4 className="bold-text">{workItem?.title}</h4>
